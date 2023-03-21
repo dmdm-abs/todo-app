b6dadd32852d7e572f5caedb90bda97a07000e30
dmdm-abs <dmdm@abstractit.in>
2023-03-21 18:28:10 +0530

    Seperated SelectAllTodos component.

---
+++ src/components/SelectAllTodos.js
@@ -0,0 +1,19 @@
+import { peek } from '@laufire/utils/debug';
+import React from 'react';
+import TodoManager from '../services/TodoManager';
+
+const SelectAllTodos = (context) => {
+	const { actions: { changeAllStatus }} = context;
+
+	return <div>
+		<input
+			type="checkbox"
+			name="Select All"
+			onChange={ ({ target: { checked }}) => changeAllStatus(checked) }
+			checked={ peek(TodoManager.checkAllStatus(context)) }
+		/>
+		<label id="Select All">Select All</label></div>;
+}
+	;
+
+export default SelectAllTodos;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,19 +1,16 @@
 import { map } from '@laufire/utils/collection';
 import React from 'react';
-import TodoManager from '../services/TodoManager';
+import ClearButton from './ClearButton';
+import SelectAllTodos from './SelectAllTodos';
 import Todo from './Todo';
 
 const Todos = (context) => {
-	const { state: { todos }, actions: { changeAllStatus }} = context;
+	const { state: { todos }} = context;
 
 	return (
 		<div>
-			<input
-				type="checkbox"
-				onChange={ ({ target: { checked }}) =>
-					changeAllStatus(checked) }
-				checked={ TodoManager.checkAllStatus(context) }
-			/>
+			<SelectAllTodos { ...context }/>
+			<ClearButton { ...context }/>
 			{map(todos, (todo, key) =>
 				<Todo key={ key } { ...{ ...context, data: { todo }} }/>)}
 		</div>
