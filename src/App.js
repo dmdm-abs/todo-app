5f397445ccd1695d9aacdbd7421c6006c7bd7a07
dmdm-abs <dmdm@abstractit.in>
2023-03-27 16:21:31 +0530

    Introduced TasksPane and Title.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { React } from 'react';
 import './App.scss';
+import TasksPane from './components/TasksPane';
 import TodosPane from './components/TodosPane';
 
 const App = (context) =>
 	<div className="App">
 		<TodosPane { ...context }/>
+		<TasksPane { ...context }/>
 	</div>;
 
 export default App;
---
+++ src/components/TasksPane/Title.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Title = () => <h2>Tasks</h2>;
+
+export default Title;
