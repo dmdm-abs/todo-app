884639c4c42397c8bb5ace2039041effae98e00d
dmdm-abs <dmdm@abstractit.in>
2023-03-27 13:12:21 +0530

    Bug fixed.

--- src/components/TodosPane/TodoActions/SelectAllTodos.js
+++ src/components/TodosPane/TodoActions/SelectAllTodos.js
@@ -4,13 +4,13 @@ import TodoManager from '../../../services/TodoManager';
 const SelectAllTodos = (context) => {
 	const { actions: { toggleAllStatus }} = context;
 
-	return <div>
+	return <span>
 		<input
 			type="checkbox"
 			onChange={ ({ target: { checked }}) => toggleAllStatus(checked) }
 			checked={ TodoManager.checkAllStatus(context) }
 		/>
-		<label>Select All</label></div>;
+		<label>Select All</label></span>;
 };
 
 export default SelectAllTodos;
